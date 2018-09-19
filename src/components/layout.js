1c225467d9449bcf46cb5a16137cd0a6de8573c6
blubMe <fahmiirsyad10@gmail.com>
2018-09-19 11:38:28 +0700

    refactor : refactor header head content

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,8 +22,8 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'my exclusive blog created with ❤️' },
+            { name: 'keywords', content: 'fahmiirsyd, programming' },
           ]}
         >
           <html lang="en" />
